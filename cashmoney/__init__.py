f85ddc32e8b0470015eb4534b699b9b66ce12d7d
Britni Canale <bcanale@stuy.edu>
2019-05-17 12:37:06 -0400

    Beginning front-end

--- cashmoney/__init__.py
+++ cashmoney/__init__.py
@@ -3,10 +3,18 @@ from flask import Flask, render_template, session, request, url_for, redirect, f
 
 app = Flask(__name__)
 
+projects = []
+for i in range(0,10):
+    project = {}
+    project['title'] = "doggo ipsum"
+    project['img'] = "https://img.buzzfeed.com/buzzfeed-static/static/2017-03/21/7/asset/buzzfeed-prod-fastlane-02/sub-buzz-668-1490096330-22.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto"
+    project['description'] = "Doggo ipsum he made many woofs shoob yapper, you are doing me a frighten. I am bekom fat blep doggo very taste wow boof, I am bekom fat waggy wags clouds ur givin me a spook porgo, heckin angery woofer doing me a frighten you are doin me a concern."
+    projects[i] = project
+
 @app.route("/")
 def hello():
     print ("hello there")
-    return render_template("home.html")
+    return render_template("home.html", feed = projects)
 
 
 if __name__ == "__main__":

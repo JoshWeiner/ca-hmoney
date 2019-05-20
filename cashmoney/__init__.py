8fa94675070710bcb3cfbc456109f5a7670a70d5
Britni <bcanale@stuy.edu>
2019-05-20 12:33:28 -0400

    Continuing front-end {

--- cashmoney/__init__.py
+++ cashmoney/__init__.py
@@ -9,7 +9,8 @@ for i in range(0,10):
     print(i)
     project = {}
     project['title'] = "doggo ipsum"
-    project['img'] = "https://img.buzzfeed.com/buzzfeed-static/static/2017-03/21/7/asset/buzzfeed-prod-fastlane-02/sub-buzz-668-1490096330-22.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto"
+    #project['img'] = "https://img.buzzfeed.com/buzzfeed-static/static/2017-03/21/7/asset/buzzfeed-prod-fastlane-02/sub-buzz-668-1490096330-22.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto"
+    project['img'] = "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg"
     project['description'] = "Doggo ipsum he made many woofs shoob yapper, you are doing me a frighten. I am bekom fat blep doggo very taste wow boof, I am bekom fat waggy wags clouds ur givin me a spook porgo, heckin angery woofer doing me a frighten you are doin me a concern."
     print(project)
     projects.append(project)

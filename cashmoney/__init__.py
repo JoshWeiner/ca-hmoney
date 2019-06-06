f79ba2f84d07d98bb988d209feecd0114d098913
JoshWeiner <joshuaweinernyc@gmail.com>
2019-06-06 13:17:52 -0400

    Update __init__.py

--- cashmoney/__init__.py
+++ cashmoney/__init__.py
@@ -24,6 +24,7 @@ import uuid
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    SECRET_KEY = os.urandom(32)
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
